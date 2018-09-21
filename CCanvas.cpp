8cdddc49cc3b4f5834dc88441d9c5bc5e445fb98
Victor Hugo Flores Marquez <35241075+lGaze@users.noreply.github.com>
2018-09-20 19:11:20 -0500

    avances

--- CCanvas.cpp
+++ CCanvas.cpp
@@ -20,64 +20,114 @@ bool CCanvas::init(int w, int h, int p)
 	m_width = w;
 	m_height = h;
 	m_pitch = p;
+	m_lengh = m_width * m_pitch;
 	buffer = new unsigned char[m_width*m_height*m_pitch];
-
 	return true;
 }
 
 /**
  * 
  */
-void CCanvas::set(int refer, unsigned char info)
+void CCanvas::set(int x, int y, int canal, unsigned char info)
 {	
-	buffer[refer] = info;
+	if (y >= m_width)
+	{
+		cout << "Fuera de rango" << endl;
+		
+	}
+
+	else if (x >= m_height)
+	{
+		cout << "Fuera de rango" << endl;
+
+	}
+	else
+	{
+		int block = (x*m_width) + y;
+		buffer[block + canal] = info;
+	}
+		
+	
 }
 
 /**
  * 
  */
-int CCanvas::get(int x, int y)
+void CCanvas::set(unsigned char * inicio, int canal, unsigned char info)
+{
+	for (int i = 0; i<canal; i++)
+	{
+		*inicio++;
+	}
+
+	*inicio = info; 
+
+}
+
+/**
+ * 
+ */
+unsigned char * CCanvas::get(int x, int y)
 {
 	if (y >= m_width)
 	{
-		cout << "fuera de rango";
-		return 0;
+		cout << "Fuera de rango" << endl;
+		return nullptr;
 	}
-	else if (x>= m_height)
+	else if (x >= m_height)
 	{
-		cout << "fuera de rango";
-		return 0;
-	}
-
-	int blockStart = (x*m_width) + y;
-	blockStart = blockStart * m_pitch;
-	return blockStart;
-}
-
-void CCanvas::drawLine(int xi, int yi, int xf, int yf)
-{
-	bool isHorizontal = false;
-
-	int start = this->get(xi,yi);
-	int finish = this->get(xf, yf);
-	
-	if (xi - xf == 0)
-	{
-		isHorizontal = true;
-	}
-
-	if (isHorizontal)
-	{
-		int len = (finish - start) + m_pitch;
-		for (int i = 0; i < len; i++)
-		{
-			this->set(start + i, '1');
-		}
+		cout << "Fuera de rango" << endl;
+		return nullptr;
 	}
 	else
 	{
+		unsigned char * inicio = &buffer[((x*m_width) + y) * m_pitch];
+		return inicio;
+	}
+		
+}
+
+/**
+ * 
+ */
+void CCanvas::drawLine(int xi, int yi, int xf, int yf, unsigned char info)
+{
+	bool isHorizontal = false;
+	if (yi >= m_width || yf >= m_width)
+	{
+		cout << "Fuera de rango" << endl;
 		
 	}
+	else if (xi >= m_height || xf >=m_height)
+	{
+		cout << "Fuera de rango" << endl;
+		
+	}
+	else
+	{
+		if (xi - xf == 0)
+		{
+			isHorizontal = true;
+		}
+
+		if (isHorizontal)
+		{
+			int len = (yf - yi) * m_pitch;
+			for (int i = 0; i < len + 4; i++)
+			{
+				this->set(this->get(xi, yi), i, info);
+			}
+		}
+		else
+		{  
+
+			for (int i = yi; i < yf; i++)
+			{
+			  
+			}
+		}
+	}
+	
 }
 
 void CCanvas::copy(void * dest, const void * src, size_t siz)
--- CCanvas.h
+++ CCanvas.h
@@ -6,9 +6,10 @@ public:
 	~CCanvas();
 
 	bool init(int w, int h, int p);
-	void set(int refer, unsigned char info);
-	int get(int x, int y);
-	void drawLine(int xi, int yi, int xf, int yf); 
+	void set(int x , int y, int canal, unsigned char info);
+	void set(unsigned char * inicio, int canal, unsigned char info);
+	unsigned char *  get(int x, int y);
+	void drawLine(int xi, int yi, int xf, int yf, unsigned char info); 
 	void copy(void * dest, const void * src, size_t siz);
 	unsigned char * buffer;
 
@@ -17,5 +18,8 @@ private:
 	int m_width;
 	int m_height;
 	int m_pitch;
+	int m_lengh;
+	int m_widthNormal;
+	int m_heightNormal;
 };
 
