a35f8b3c04a799ebab7e74a567232fab14bae48a
Victor Hugo Flores Marquez <35241075+lGaze@users.noreply.github.com>
2018-09-28 16:51:41 -0500

    Avances

--- CCanvas.cpp
+++ CCanvas.cpp
@@ -13,125 +13,146 @@ CCanvas::CCanvas()
 
 CCanvas::~CCanvas()
 {
+
 }
 
-bool CCanvas::init(int w, int h, int p)
+bool CCanvas::init(int w, int h, int f)
 {
+	m_format = f;
 	m_width = w;
 	m_height = h;
-	m_pitch = p;
+	m_pitch = w * f;
 	m_lengh = m_width * m_pitch;
 	buffer = new unsigned char[m_width*m_height*m_pitch];
 	return true;
 }
 
-/**
- * 
- */
-void CCanvas::set(int x, int y, int canal, unsigned char info)
-{	
-	if (y >= m_width)
-	{
-		cout << "Fuera de rango" << endl;
-		
-	}
-
-	else if (x >= m_height)
-	{
-		cout << "Fuera de rango" << endl;
-
-	}
-	else
-	{
-		int block = (x*m_width) + y;
-		buffer[block + canal] = info;
-	}
-		
+void CCanvas::set(float U, float V, const unsigned char *Destiny)
+{ 
 	
-}
-
-/**
- * 
- */
-void CCanvas::set(unsigned char * inicio, int canal, unsigned char info)
-{
-	for (int i = 0; i<canal; i++)
-	{
-		*inicio++;
-	}
-
-	*inicio = info; 
-
-}
-
-/**
- * 
- */
-unsigned char * CCanvas::get(int x, int y)
-{
-	if (y >= m_width)
+	if (U > 1.0f|| U< 0.0f || V > 1.0f || V < 0.0f)
 	{
 		cout << "Fuera de rango" << endl;
-		return nullptr;
-	}
-	else if (x >= m_height)
-	{
-		cout << "Fuera de rango" << endl;
-		return nullptr;
 	}
 	else
 	{
-		unsigned char * inicio = &buffer[((x*m_width) + y) * m_pitch];
-		return inicio;
+		int x = m_height * U;
+		int y = m_width *  V;
+		for (int i = 0; i<m_format; i++)
+		{
+		  buffer[this->jump(x, y)+i] = Destiny[i];
+		}
+	}
+}
+
+void CCanvas::get(float U, float V, unsigned char *Result)
+{
+	if (U > 1.0f || U < 0.0f || V > 1.0f || V < 0.0f)
+	{
+		cout << "Fuera de rango" << endl;
+	}
+	else
+	{
+		int x = U * m_height;
+		int y = V * m_width;
+		for (int i = 0; i < m_format; i++)
+		{
+			Result[i] = buffer[this->jump(x, y)+i];
+		}
 	}
 		
 }
 
-/**
- * 
- */
-void CCanvas::drawLine(int xi, int yi, int xf, int yf, unsigned char info)
+void CCanvas::drawLine(float Ui, float Vi, float Uf, float Vf)
 {
 	bool isHorizontal = false;
-	if (yi >= m_width || yf >= m_width)
+	if (Ui > 1.0f || Ui < 0.0f || Vi > 1.0f || Vi < 0.0f || Uf > 1.0f || Uf < 0.0f || Vf>1.0f || Vf < 0.0f)
 	{
 		cout << "Fuera de rango" << endl;
-		
 	}
-	else if (xi >= m_height || xf >=m_height)
-	{
-		cout << "Fuera de rango" << endl;
-		
-	}
+
 	else
 	{
-		if (xi - xf == 0)
+		int xi = Ui * m_height;
+		int yi = Vi * m_width;
+		int xf = Uf * m_height;
+		int yf = Vf * m_width;
+		int delta;
+
+		if (Ui - Uf == 0)
 		{
 			isHorizontal = true;
+			int puntoInicial = jump(xi, yi);
+			int puntoFinal = jump(xf, yf);
+			delta = puntoFinal - puntoInicial;
 		}
 
 		if (isHorizontal)
 		{
-			int len = (yf - yi) * m_pitch;
-			for (int i = 0; i < len + 4; i++)
+			for (int i = 0; i<delta; i++)
 			{
-				this->set(this->get(xi, yi), i, info);
+				buffer[jump(xi,yi)+ i] = '1';
 			}
 		}
 		else
-		{  
+		{
+			int puntoInicial = jump(xi, yi);
+			int puntoFinal = jump(xf, yf);
 
-			for (int i = yi; i < yf; i++)
+			for (int i = puntoInicial; i< puntoFinal ; i++)
 			{
-			  
+				puntoInicial = i;
+
+				for (int j = 0; j < m_format; j++)
+				{
+					buffer[puntoInicial + j] = '1';
+				}
+				i += m_pitch-1;
 			}
 		}
 	}
 	
+}
+
+int CCanvas::jump(int x, int y)
+{
+	int salto = (x * m_pitch) + (y * m_format);
+	return salto;
 }
 
 void CCanvas::copy(void * dest, const void * src, size_t siz)
 {
 	memcpy(dest, src, siz);
 }
+
+
+//Sin terminar.
+void CCanvas::drawLineMath(float Ui, float Vi, float Uf, float Vf)
+{
+	if (Ui > 1.0f || Ui < 0.0f || Vi > 1.0f || Vi < 0.0f || Uf > 1.0f || Uf < 0.0f || Vf>1.0f || Vf < 0.0f)
+	{
+		cout << "Fuera de rango" << endl;
+	}
+	float deltaX = Ui - Uf;
+	float deltaY = Vi - Vf;
+	float m;
+	float magnitud;
+	int xi = m_height * Ui;
+	int yi = m_width * Vi;
+	int xf = m_height * Uf;
+	int yf = m_width * Vf;
+	
+
+
+	if (deltaX > deltaY)
+	{
+		m = deltaX / deltaY;
+	}
+	else
+	{
+		m = deltaY / deltaX;
+	}
+}
+
+
 
--- CCanvas.h
+++ CCanvas.h
@@ -1,25 +1,58 @@
 #pragma once
+
+enum FORMATO
+{
+	R8G8B8A8 = 0x01,
+	R8G8B8 = 0x02,
+	R8G8 = 0x03,
+	R8 = 0x04,
+};
 class CCanvas
 {
 public:
 	CCanvas();
 	~CCanvas();
 
-	bool init(int w, int h, int p);
-	void set(int x , int y, int canal, unsigned char info);
-	void set(unsigned char * inicio, int canal, unsigned char info);
-	unsigned char *  get(int x, int y);
-	void drawLine(int xi, int yi, int xf, int yf, unsigned char info); 
-	void copy(void * dest, const void * src, size_t siz);
-	unsigned char * buffer;
+	/** Inicializa las variables y el buffer.
+	*  Recibe un entero whidth , un entero height y un formato.
+	*/
+	bool init(int w, int h, int f);
 
+	/** Setea informacion dentro del buffer.
+	* 
+	*/
+	void set(float x, float y, const unsigned char *Destiny);
+
+	/** Obtiene la referencia de memoria del inicio del bloque.
+	* Recibe cordenadas X y Y
+	*/
+	void get(float x, float y,  unsigned char *Result);
+
+	/** Dibuja una linea ya sea horizontal o vetical.
+	*   Recibe cordenadas del punto inicial y del punto final y la informacion a setear.
+	*/
+	void drawLine(float xi, float yi, float xf, float yf); 
+
+	/** Copia la informacion de un buffer a otro
+	* Recibe un puntero de destino, un puntero constate del souce y un tamaño.
+	*/
+	void copy(void * dest, const void * src, size_t siz);
+	/** Calcula saltos en el buffer.
+	*  Recibe cordenadas X,Y y regresa el salto 
+	*/
+	int jump(int x, int y);
+	/**
+	 * 
+	 */
+	void drawLineMath(float xi, float yi, float xf, float yf);
+	unsigned char * buffer;
+	
 private:
 
 	int m_width;
 	int m_height;
 	int m_pitch;
 	int m_lengh;
-	int m_widthNormal;
-	int m_heightNormal;
+	int m_format;
 };
 
